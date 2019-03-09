b03e3518b26bbcf4eb1f5e9be46ae3415c4d5a1d
shankar7042 <shankar.kumar.4171@gmail.com>
2019-03-09 16:35:08 +0530

    Smooth Scroll Effect

---
+++ main.js
@@ -0,0 +1,22 @@
+// option 2
+
+// $('.navbar a').on('click', function(e) {
+//   if (this.hash !== '') {
+//     e.preventDefault();
+
+//     const hash = this.hash;
+
+//     $('html, body ').animate(
+//       {
+//         scrollTop: $(hash).offset().top
+//       },
+//       800
+//     );
+//   }
+// });
+
+// option 3
+
+const scroll = new SmoothScroll('.navbar a[href*="#"]', {
+  speed: 600
+});
